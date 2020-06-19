76c8918019a2cd901cb82d7ddc6340e06bd096bb
Mathieu Termet <mathieu.termet@gmail.com>
2020-06-19 10:05:57 +0200

    Début d'ajout de la connexion

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-
+app.set('view-engine', 'ejs')
 router.get('/', function(requestHTTP, responseHTTP, next){
 	responseHTTP.render('home.ejs')
 })
+
+router.get('/login', function(requestHTTP, responseHTTP, next){
+	responseHTTP.render('login.ejs')
+})
+router.get('/register', function(requestHTTP, responseHTTP, next){
+	responseHTTP.render('register.ejs')
+})
+
+router.post('/register', function(requestHTTP, responseHTTP, next){
+ // traitement
+})
+
+router.post('/login', function(requestHTTP, responseHTTP, next){
+// traitement
+})
+
 
 app.use('/',router)
 app.use(express.static(__dirname + '/public'))
